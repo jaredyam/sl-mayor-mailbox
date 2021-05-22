1f8b016de982a4feff168868f857832583709fff
jaredyam <jihonren@gmail.com>
2021-05-22 17:43:34 +0800

    Reorder agencies by comparing number of reply mails

--- scripts/generate_reply_agency_mds.py
+++ scripts/generate_reply_agency_mds.py
@@ -12,10 +12,10 @@ def generate_reply_agency_mds():
     df = df_expand_list_to_rows(
         df.assign(**{'reply_agency': df['reply_agency'].str.split()}),
         'reply_agency')
-    agencies = list(dict.fromkeys(df['reply_agency']))
+    count_agency_mails = dict.fromkeys(df['reply_agency'])
 
     AGENCY_PATH.mkdir(parents=True, exist_ok=True)
-    for agency in agencies:
+    for agency in count_agency_mails.keys():
         group_rows = df.loc[df.reply_agency == agency][['title', 'url']]
         with open(AGENCY_PATH / f'{agency}.md', 'w') as md:
             md.write(f'# {agency}\n')
@@ -27,20 +27,21 @@ def generate_reply_agency_mds():
                     file=f'{get_mail_id_from_url(row["url"])}.md'))
                 mails.append(f'- {markdown_link(row["title"], filepath)}')
 
+            count_agency_mails[agency] = len(mails)
             md.write(f'__TOTAL MAILS : {len(mails)}__\n')
             md.write('\n'.join(mails))
 
     with open(AGENCY_PATH / 'README.md', 'w') as md:
         md.write(f'# 👮‍♀️ Agencies\n')
-        md.write(f'__TOTAL AGENCIES : {len(agencies)}__\n')
-        for agency in agencies:
+        md.write(f'__TOTAL AGENCIES : {len(count_agency_mails.keys())}__\n')
+        for agency in sorted(count_agency_mails.keys(),
+                             key=lambda x: count_agency_mails[x],
+                             reverse=True):
             md.write(f'- {markdown_link(agency, f"{agency}.md")}\n')
 
 
 def df_expand_list_to_rows(df, column):
-    """Expand dataframe cells of list type to multiple rows, then each row
-    contains one item in the list.
-    """
+    """Expand dataframe cells of list type to multiple rows."""
     return pd.DataFrame({c: np.repeat(df[c].values,
                                       df[column].str.len())
                          for c in df.columns.difference([column])}
