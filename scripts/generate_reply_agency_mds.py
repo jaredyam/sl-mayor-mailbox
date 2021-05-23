1f60687b665b2717e1ef245ae138bc4fe7821e05
jaredyam <jihonren@gmail.com>
2021-05-23 09:54:58 +0800

    Display agency-specific mails in table format

--- scripts/generate_reply_agency_mds.py
+++ scripts/generate_reply_agency_mds.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 from utils import (LATEST_DATA, AGENCY_PATH, MAIL_PATH, PROJECT_PATH,
-                   get_mail_id_from_url, markdown_link)
+                   get_mail_id_from_url, markdown_link, html_link)
 csv_converter = importlib.import_module('csv-converter.csv_converter')
 
 
@@ -18,20 +18,24 @@ def generate_reply_agency_mds():
 
     AGENCY_PATH.mkdir(parents=True, exist_ok=True)
     for agency in count_agency_mails.keys():
-        group_rows = df.loc[df.reply_agency == agency][['title', 'url']]
+        agency_df = df.loc[df['reply_agency'] == agency]
+        agency_df = agency_df[['query_date', 'reply_date', 'title', 'url']]
         with open(AGENCY_PATH / f'{agency}.md', 'w') as md:
             md.write(f'# {agency}\n')
 
             mails = []
-            for i, row in group_rows.iterrows():
+            for i, row in agency_df.iterrows():
                 filepath = Path('{path}/{file}'.format(
                     path=f'../../{MAIL_PATH.relative_to(PROJECT_PATH)}',
                     file=f'{get_mail_id_from_url(row["url"])}.md'))
-                mails.append(f'- {markdown_link(row["title"], filepath)}')
+                mails.append(f'- {html_link(row["title"], filepath)}<br/>'
+                             f'  Query Date : {"<b>"+str(row["query_date"])+"</b>":<17} - '
+                             f'Reply Date : {"<b>"+str(row["reply_date"])+"</b>":<17}')
 
             count_agency_mails[agency] = len(mails)
-            md.write(f'__TOTAL MAILS : {len(mails)}__\n')
+            md.write(f'<pre><b>TOTAL MAILS : {len(mails)}</b></pre>\n<pre>\n')
             md.write('\n'.join(mails))
+            md.write('\n</pre>')
 
     with open(AGENCY_PATH / 'README.md', 'w') as md:
         md.write(f'# 👮‍♀️ Agencies\n')
